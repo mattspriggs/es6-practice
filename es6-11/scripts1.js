fe0a41f87dadc3b04a324784f68c89c2ed36335e
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-27 23:32:08 +1300

    Completed arrow exercises

--- es6-11/scripts1.js
+++ es6-11/scripts1.js
@@ -1,11 +1,18 @@
 // Select all the list items on the page and convert to array
-
+let list = Array.from(document.querySelectorAll('li'))
+console.log(list)
 // Filter for only the elements that contain the word 'Flexbox'
-
+const flexbox = list
+	.filter(item => item.textContent.includes('Flexbox'))
 // map down to a list of time strings
-
+	.map(item => item.dataset.time)
 // map to an array of seconds
-
+	.map(timeCode => {
+		const parts = timeCode.split(':').map(part => parseFloat(part))
+		return (parts[0]*60) + parts[1]
+	})
 // reduce to get total
+	.reduce((runningTotal, seconds) => runningTotal + seconds,0)
 
 // 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
+console.log(flexbox)
