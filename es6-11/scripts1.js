7261611413c17082f019ea403f55bdf4ffb2a2db
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-27 18:30:33 +1300

    Setup exercises with arrow codes

---
+++ es6-11/scripts1.js
@@ -0,0 +1,11 @@
+// Select all the list items on the page and convert to array
+
+// Filter for only the elements that contain the word 'Flexbox'
+
+// map down to a list of time strings
+
+// map to an array of seconds
+
+// reduce to get total
+
+// 🔥 This can also be done in a single .reduce(), but we're practicing arrow functions here, so chain them!
