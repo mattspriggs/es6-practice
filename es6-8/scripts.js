a1d0a722f57b6c9f059198eea8191b99be530313
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-27 17:35:59 +1300

    Finished work on 'this'

--- es6-8/scripts.js
+++ es6-8/scripts.js
@@ -1,4 +1,13 @@
 const box = document.querySelector('.box')
 box.addEventListener('click', function (){
-	console.log(this)
+	//console.log(this)//An arrow function will inherit the parent's binding of this
+	let first = 'opening'
+	let second = 'open'
+	if(this.classList.contains(first)) {
+		[first, second] = [second, first]//will switch the order of the functions
+	}
+	this.classList.toggle(first)
+	setTimeout(() => {
+		this.classList.toggle(second)//here use an arrow function so that this is not reassigned in a new function
+	}, 500)
 })
