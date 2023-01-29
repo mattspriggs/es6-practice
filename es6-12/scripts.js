3a7cfa66904c8f620f46b99cdc789cb2254cef96
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-29 21:28:13 +1300

    Adding the ability to add markup with template strings

--- es6-12/scripts.js
+++ es6-12/scripts.js
@@ -4,4 +4,21 @@ const sentence = `My dog ${name} is ${age * 7} years old`
 
 console.log(sentence)
 
-const
+const person = {
+	name: 'Matt',
+	job: 'Software Developer',
+	city: 'Dunedin',
+	bio: 'Matt is trying to break into software development - either web or games - and will be doing a post grad cert in IT at Otago Polytech'
+}
+
+const markup = `
+<div class="person">
+	<h2>${person.name}
+		<span class="job">${person.job}</span>
+	</h2>
+	<p class="location">${person.city}</p>
+	<p class="bio">${person.bio}</p>
+</div>
+`
+
+document.body.innerHTML = markup
