3a9d9f65203acac8012032805f694e696cc9eac5
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-29 21:38:32 +1300

    Added list items from template strings

--- es6-12/scripts.js
+++ es6-12/scripts.js
@@ -22,3 +22,17 @@ const markup = `
 `
 
 document.body.innerHTML = markup
+
+const dogs = [
+	{name: 'Snickers', age: 2},
+	{name: 'Hugo', age: 8},
+	{name: 'Sunny', age: 1},
+]
+
+const markup2 = `
+	<ul class="dogs">
+		${dogs.map(dog => `<li>${dog.name} is ${dog.age * 7} dog years old</li>`).join('')}
+	</ul>
+`
+document.body.innerHTML = markup2
+
