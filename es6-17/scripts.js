596e27d23491f3dd47aa2300c900bb00b4b3d8ac
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-30 16:40:54 +1300

    Working with multiple reg ex

--- es6-17/scripts.js
+++ es6-17/scripts.js
@@ -7,10 +7,14 @@ const model = 'X5'
 const colour = 'Royal Blue'
 
 //.startsWith()
-console.log(course.startsWith('RFB'))
-
+console.log(course.startsWith('RFB'))//Searches at the beginning
+console.log(flightNumber.startsWith('AC'))//Searches at the beginning
+console.log(flightNumber.startsWith('AC', 3))//allows you to skip a certain number of characters
 //.endsWith()
-
+console.log(flightNumber.endsWith('jz'))
+console.log(accountNumber.endsWith('RT'))
+console.log(accountNumber.endsWith('RT', 11))
 //.includes()
-
+console.log(flightNumber.includes('AC'))
+console.log(flightNumber.includes('ac'))//case-sensitive so will be false
 //.repeat()
