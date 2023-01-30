52877ecd3b32cacc717b79c3b6befcef7f8858c1
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-30 16:32:52 +1300

    Working with functions for reg ex

---
+++ es6-17/scripts.js
@@ -0,0 +1,16 @@
+const course = 'RFB2'
+const flightNumber = '20-AC2018-jz'
+const accountNumber = '825242631RT0001'
+
+const make = 'BMW'
+const model = 'X5'
+const colour = 'Royal Blue'
+
+//.startsWith()
+console.log(course.startsWith('RFB'))
+
+//.endsWith()
+
+//.includes()
+
+//.repeat()
