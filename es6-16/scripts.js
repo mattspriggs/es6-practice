6e81dc307734fd74a7f3fe2210fb6204e6c4d682
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-30 16:24:01 +1300

    Added the ability to sanitize user input

--- es6-16/scripts.js
+++ es6-16/scripts.js
@@ -1,7 +1,12 @@
+function sanitize(strings, ...values){
+	const dirty = strings.reduce((prev, next, i) => `${prev}${next}${values[i] || ''} `, '')
+	return DOMPurify.sanitize(dirty)
+}
+
 const first = 'Wes';
 const aboutMe = `I love to do evil <img src="http://unsplash.it/100/100?random" onload="alert('you got hacked');" />`;
 
-const html = `
+const html = sanitize`
 <h3>${first}</h3>
 <p>${aboutMe}</p>
 `
