63a85587add797afb7387404255ce58921ec9a5c
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-31 14:32:13 +1300

    Destructuring and default values

--- es6-20/scripts.js
+++ es6-20/scripts.js
@@ -22,3 +22,11 @@ function convertCurrency(amount){
 const { USD, AUD} = convertCurrency(100)//can also deconstruct for just some of the values in the object
 console.log(USD,  AUD)
 
+function tipCalc({total = 100, tip = 0.15, tax = 0.13}){//Using the curly brackets for the parameters allows for each to be a discrete variable of total, tip and tax
+		return total + (tip * total) + (tax * total)
+}
+const bill = tipCalc({tip: 0.2, total: 200, tax: 0.13 })//by destructuring the object you can pass in all, some or one variable - that has a default - and get it to run
+const bill2 = tipCalc({tip: 0.2, total: 200 })
+const bill3 = tipCalc({ })
+
+console.log(bill, bill2, bill3)
