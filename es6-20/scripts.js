f452dbc84629e9c9862cfdf53703ef89620522a4
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-31 14:15:20 +1300

    Added currency converter to work on destructuring from an object

--- es6-20/scripts.js
+++ es6-20/scripts.js
@@ -6,3 +6,19 @@ console.log(inRing, side);
 // side = inRing
 // inRing = side
 console.log(inRing, side)
+
+function convertCurrency(amount){
+		const converted = {
+			USD: amount * 0.76,
+			GPB: amount * 0.53,
+			AUD: amount * 1.01,
+			MEX: amount * 13.30
+		}
+		return converted
+}
+
+// const { USD, GPB, AUD, MEX } = convertCurrency(100)
+// console.log(USD, GPB, AUD, MEX) - can destructure for all values in the object
+const { USD, AUD} = convertCurrency(100)//can also deconstruct for just some of the values in the object
+console.log(USD,  AUD)
+
