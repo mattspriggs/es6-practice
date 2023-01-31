f1e708b2ac837eafebbb88b1474383efd7661039
Matt Spriggs <mspriggs1@cnm.edu>
2023-01-31 16:22:24 +1300

    Finished work with array.from

--- es6-25/scripts.js
+++ es6-25/scripts.js
@@ -1,2 +1,24 @@
-const people = document.querySelectorAll('.people p')
-console.log(people)
+// const people = document.querySelectorAll('.people p')
+// const peopleArray = Array.from(people)//takes an array-ish (node list) and converts to an array
+// console.log(peopleArray)
+// const names = peopleArray.map(person => person.textContent)
+// console.log(names)
+//REFACTORED:
+// const people = Array.from(document.querySelectorAll('.people p'))
+// const names = people.map(person => person.textContent)
+// console.log(names)
+//REFACTORED AGAIN
+// const people = document.querySelectorAll('.people p')
+// const peopleArray = Array.from(people, person => {
+// 	console.log(person)
+// 	return person.textContent
+//
+// })
+// console.log(peopleArray)
+
+function sumAll(){
+	const nums = Array.from(arguments)
+	return nums.reduce((prev,next) => prev + next, 0)
+}
+
+sumAll(1,23,242,45,654,378,99,990)
