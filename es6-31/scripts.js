830c9c5656ad88031f7e8e61a34634bbd1c63968
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-01 17:26:33 +1300

    Using spread to modify an array

--- es6-31/scripts.js
+++ es6-31/scripts.js
@@ -1,1 +1,7 @@
-const inventors = []
+const inventors = ['Einstein', 'Newton', 'Galileo']
+const newInventors = ['Musk', 'Jobs']
+
+//inventors.push.apply(inventors, newInventors)// Will work to add the newInventors to inventors - apply will do inventors.push('Musk', 'Jobs') essentially for each item - it applies push to each array item
+
+inventors.push(...newInventors)
+console.log(inventors)
