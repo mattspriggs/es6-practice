952eb2f5545bcba461c91c4abe4bd425a2f583e1
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-01 16:18:53 +1300

    Spread and array copies

--- es6-28/scripts.js
+++ es6-28/scripts.js
@@ -10,4 +10,8 @@ const deepDish = {
 //the ...spread operator will take anything that is iterable and apply it to the containing array
 const pizzas = [...featured, "veg", ...specialty]//will create a new array with the names from the two arrays
 const fridayPizzas = pizzas//this just creates a reference, not an isolated copy - so any change to fridayPizzas will change the referenced array of pizzas
+const thursdayPizzas = [].concat(pizzas)//makes a true copy
+const wednesdayPizzas = [...pizzas]//will also make a true copy of the array'
+
+
 console.log(pizzas)
