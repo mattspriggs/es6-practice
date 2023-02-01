23537de0d8b6bcc0f6cb97a6cf0601d4153a3e1c
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-01 16:09:43 +1300

    Working the ...spread operator

---
+++ es6-28/scripts.js
@@ -0,0 +1,13 @@
+const featured = ['Deep Dish', 'Peperoni', 'Hawaiian']
+const specialty = ['Meatzza', 'Spicy Mama', 'Margherita']
+
+const deepDish = {
+	pizzaName: 'Deep Dish',
+	size: 'Medium',
+	ingredients: ['Marinara', 'Italian Sausage', 'Dough', 'Cheese']
+}
+
+//the ...spread operator will take anything that is iterable and apply it to the containing array
+const pizzas = [...featured, "veg", ...specialty]//will create a new array with the names from the two arrays
+const fridayPizzas = pizzas//this just creates a reference, not an isolated copy - so any change to fridayPizzas will change the referenced array of pizzas
+console.log(pizzas)
