3debac74f420363eea913ff3e30ac9c8693f7102
Matt Spriggs <mspriggs1@cnm.edu>
2023-02-03 15:17:23 +1300

    Added work on creating a promise

--- es6-34/scripts.js
+++ es6-34/scripts.js
@@ -5,3 +5,21 @@ postsPromise
 	.then(data => data.json())
 	.then(data => {console.log(data)})//.then()will only run when the data comes back
 	.catch(err => {console.error(err)})
+
+//Create promises
+
+const p = new Promise((resolve, reject) =>{
+	setTimeout(() =>{
+	// resolve('Matt is cool')
+		reject(Error('Err Matt isn\'t cool'))
+	},2000)
+
+})
+
+p
+	.then(data => {
+		console.log(data)
+	})
+	.catch(err =>{
+		console.error(err)
+	})
